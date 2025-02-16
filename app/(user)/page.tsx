668baa1809980174ec4f9db5a24b713a404c52a8
shandohowell <shandohowell94@gmail.com>
2025-02-15 19:52:14 -0500

    Working on home page.

---
+++ app/(user)/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background">
+      <Hero/>
+      Welcome
+    </div>
+  );
+}
